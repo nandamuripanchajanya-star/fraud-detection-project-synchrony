6b98aaa89f1393e023bf1337c3c8c1e7a86b6eba
nandamuripanchajanya-star <nandamuripanchajanya@gmail.com>
2026-08-21 00:55:23 +0530

    Allow production frontend CORS

--- backend/main.py
+++ backend/main.py
@@ -75,8 +75,9 @@ async def log_requests(request, call_next):
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-    "http://localhost:5173",
-    "http://localhost:5174",
+        "http://localhost:5173",
+        "http://localhost:5174",
+        "https://fraudsense-frontend-fcif.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
